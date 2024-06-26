6311615b71fa2a3472417a3d7794d9c0b64f4b5d
Jfloh <jfloh2005@gmail.com>
2024-06-26 19:39:39 +0800

    TESTING number 1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("SIMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM")
+def main_m():
+    function = int(input("Welcome to KLCCC automated system\n1: Sign up\n2: Login\n3: Exit\n"))
+    while function < 1 or function > 3: #validate the option
+        function = int(input("Invalid option\n\n1: Sign up\n2: Login\n3: Exit\n"))
+    return function
+main_m()
