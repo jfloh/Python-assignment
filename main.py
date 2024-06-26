9997d71c984f9427275eb597924e1433a01feb59
Jfloh <jfloh2005@gmail.com>
2024-06-26 19:40:52 +0800

    Select option added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main_m():
     function = int(input("Welcome to KLCCC automated system\n1: Sign up\n2: Login\n3: Exit\n"))
-    while function < 1 or function > 3: #validate the option
+    while function < 1 or function > 3: #validate the option(Only 1-3 can be input)
         function = int(input("Invalid option\n\n1: Sign up\n2: Login\n3: Exit\n"))
     return function
 main_m()
