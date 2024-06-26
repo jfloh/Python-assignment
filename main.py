6b5247df65039d3486b49e1b59f2881a7dabec46
ChaiTC <tianchengctc5@gmail.com>
2024-06-26 21:00:11 +0800

    ```

--- main.py
+++ main.py
@@ -10,3 +10,21 @@ def main_m():
         print("Exit selected")
         quit()
 main_m()
+
+def main_menu():
+    while True:
+        print("Welcome to KLCCC System")
+        print("1. Sign Up")
+        print("2. Login")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            user_management.sign_up()
+        elif choice == '2':
+            user_management.login()
+        elif choice == '3':
+            print("Exiting the system. Goodbye!")
+        else:
+            print("Invalid choice. Please try again!")
+main_menu()
