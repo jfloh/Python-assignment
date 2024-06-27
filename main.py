3d4455f890a8f12ac8a2f9c3b68522e62929cd5b
Jfloh <jfloh2005@gmail.com>
2024-06-27 14:31:30 +0800

    Login system

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sp_user=['admin123','admin123']
+sp_user=['admin','admin123']
 def main_menu():
     while True:
         print("Welcome to KLCCC System")
@@ -10,7 +10,9 @@ def main_menu():
         if choice == '1':
             user_management.sign_up()
         elif choice == '2':
-            user_management.login()
+            login_sys()
+            break
+            #user_management.login()
         elif choice == '3':
             print("Exiting the system. Goodbye!")
             break
@@ -18,7 +20,9 @@ def main_menu():
         else:
             print("Invalid choice. Please try again!\n")
 def login_sys():
-#Login system here
-def signup_sys():
+    login_name=input("Enter username: ")
+    if login_name in sp_user[1]:
+        print("Welcome Super user ! ")
+#def signup_sys():
 #Sign up system here
 main_menu()
