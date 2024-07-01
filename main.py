0abb03ad5b71f98fb0c1b98ea9b0cce9a2894608
Jfloh <jfloh2005@gmail.com>
2024-07-01 22:31:09 +0800

    Login system

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sp_user=['admin','admin123']
+sp_user="admin,admin123" #Define Super-user usernama & password
 def main_menu():
     while True:
         print("Welcome to KLCCC System")
@@ -21,8 +21,20 @@ def main_menu():
             print("Invalid choice. Please try again!\n")
 def login_sys():
     login_name=input("Enter username: ")
-    if login_name in sp_user[1]:
+    login_pass=input("Enter password: ")
+    sp_fields = sp_user.split(",") #Split string to Char
+    file = open("User_details.txt","r")
+    for i in file:
+        user_fields= i.split(",")
+
+    if login_name == sp_fields[0] and login_pass == sp_fields[1]: #Check name and Password
+        print("Login Successful")
         print("Welcome Super user ! ")
+        #Call function for super-user
+    elif login_name == user_fields[0] and login_pass== user_fields[1] :
+        print("Login Successful")
+    else:
+        print("Username or Password incorrect, please try again")
 #def signup_sys():
 #Sign up system here
 main_menu()
